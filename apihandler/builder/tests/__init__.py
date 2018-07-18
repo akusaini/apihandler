ce40229938424f12af2fa74d53052e0d4590661e
Akash saini <akusaini@gmail.com>
2018-07-18 13:11:57 +0530

    adding the test for the news api

--- apihandler/builder/tests/__init__.py
+++ apihandler/builder/tests/__init__.py
@@ -1,7 +1,5 @@
-from .basictest import TestBasic
 from .apinewstest import TestNewsApi
 
 __test__ = {
-	'BasicTest': TestBasic,
 	'HttpRest': TestNewsApi
 }
--- apihandler/builder/tests/apinewstest.py
+++ apihandler/builder/tests/apinewstest.py
@@ -1,13 +1,44 @@
 import unittest
 from apihandler.builder.extras.explorer import HttpJsonClient
+import urllib3
+import json
+from apihandler.apihandler.settings import NEWS_API_TOKEN
+
+http = urllib3.PoolManager()
+domain = "http://18.220.145.52:8001/v1/news"
 
 
 class TestNewsApi(unittest.TestCase):
 	def test_method_allowed(self):
-		http = HttpJsonClient("http://18.220.145.52:8901/v1/news")
-		code, result = http.post({})
-		if code == 405:
-			print(33)
+		# Filtering first non allowed methods.
+		for method in ['POST', 'PUT', 'PATCH']:
+			response = http.request(method, domain)
+			self.assertEqual(response.status, 405)
+			response_data = json.loads(response.data.decode('utf-8'))
+			self.assertEqual(response_data.get('success'), False)
+
+	def test_query_params(self):
+		# Hitting without any query params
+		url = domain
+		response = http.request('GET', '%s?apiKey=%s' % (url, NEWS_API_TOKEN) )
+		self.assertEqual(response.status, 400)
+		response_data = json.loads(response.data.decode('utf-8'))
+		self.assertEqual(response_data.get('success'), False)
+		self.assertEqual(response_data.get('message').lower(), "Bad request.".lower())
+
+
+		# Hitting with partial query params
+		response = http.request('GET', '%s?country=us&apiKey=%s' % (url, NEWS_API_TOKEN))
+		self.assertEqual(response.status, 400)
+		response_data = json.loads(response.data.decode('utf-8'))
+		self.assertEqual(response_data.get('success'), False)
+		self.assertEqual(response_data.get('message').lower(), "Bad request.".lower())
+
+		# Hitting with all required query params
+		response = http.request('GET', '%s?country=us&category=Business&apiKey=%s' % (url, NEWS_API_TOKEN))
+		self.assertEqual(response.status, 200)
+		response_data = json.loads(response.data.decode('utf-8'))
+		self.assertEqual(response_data.get('success'), True)
 
 
 if __name__ == '__main__':
